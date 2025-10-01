f67363b19f5c06e96026b7b55c91d45231956c5f
Michael Douglas <bulletcarry@gmail.com>
2025-10-01 18:40:42 -0300

    feat: setup config enviroments 2 rails application

---
+++ config/environments/development.rb
@@ -0,0 +1,59 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  config.eager_load = false
+
+  config.consider_all_requests_local = true
+
+  config.server_timing = true
+
+  if Rails.root.join("tmp/caching-dev.txt").exist?
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  config.active_storage.variant_processor = :mini_magick
+
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.active_support.deprecation = :log
+
+  config.active_support.disallowed_deprecation = :raise
+
+  config.active_support.disallowed_deprecation_warnings = []
+
+  config.active_record.migration_error = :page_load
+
+  config.active_record.verbose_query_logs = true
+
+  config.assets.quiet = true if defined?(config.assets)
+
+  config.assets.debug = true if defined?(config.assets)
+
+  config.assets.quiet = true if defined?(config.assets)
+
+  # Bullet for N+1 query detection
+  # Uncomment if using Bullet gem
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.alert = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.rails_logger = true
+  # end
+end
---
+++ config/environments/production.rb
@@ -0,0 +1,44 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+
+  config.cache_classes = true
+
+
+  config.eager_load = true
+
+  config.consider_all_requests_local       = false
+
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+
+  config.active_storage.variant_processor = :mini_magick
+
+  config.force_ssl = true
+
+  config.log_level = :info
+
+  config.log_tags = [ :request_id ]
+
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+
+
+  config.active_job.queue_adapter = :sidekiq
+
+  config.action_mailer.perform_caching = false
+
+
+  config.i18n.fallbacks = true
+
+  config.active_support.report_deprecations = false
+
+  config.log_formatter = ::Logger::Formatter.new
+
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  config.active_record.dump_schema_after_migration = false
+end
